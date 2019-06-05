eba073048c52f17bb51a8e2702930f529081e2a3
CynepHy6 <CynepHy6@gmail.com>
2019-06-06 01:41:45 +0300

    up dependency

--- config/bundles.php
+++ config/bundles.php
@@ -3,5 +3,4 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     WowApps\SlackBundle\WowAppsSlackBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
