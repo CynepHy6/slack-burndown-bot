c0ea55572d564a308ee2fb56ac37984f5fd30a76
CynepHy6 <CynepHy6@gmail.com>
2019-06-05 23:59:36 +0300

    add command

---
+++ src/Controller/BotController.php
@@ -0,0 +1,70 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use WowApps\SlackBundle\DTO\Attachment;
+use WowApps\SlackBundle\DTO\SlackMessage;
+use WowApps\SlackBundle\Service\SlackBot;
+
+class BotController
+{
+    private $bot;
+    private $rapidViewId;
+    private $sprintId;
+    private $postTime;
+
+
+    /**
+     * BotController constructor.
+     *
+     * @param $bot
+     */
+    public function __construct(SlackBot $bot)
+    {
+        $this->bot = $bot;
+    }
+
+    /**
+     * @param int $viewId
+     */
+    public function setRapidViewId(int $viewId): void
+    {
+        $this->rapidViewId = $viewId;
+    }
+
+    /**
+     * @param int $sprintId
+     */
+    public function setSprintId($sprintId): void
+    {
+        $this->sprintId = $sprintId;
+    }
+
+    /**
+     * @param string $postTime
+     */
+    public function setPostTime($postTime): void
+    {
+        $this->postTime = $postTime;
+    }
+
+    public function postBurndown(string $channel = 'my-test')
+    {
+        $imageUrl = $this->getImage();
+        $chart = new Attachment();
+        $chart->setImageUrl($imageUrl);
+        $message = new SlackMessage('Cвежий Burndown Chart');
+        $message->setChannel($channel);
+        $message->appendAttachment($chart);
+        $this->bot->send($message);
+    }
+
+    private function getImage()
+    {
+        //        TODO this
+        return '';
+    }
+
+}
